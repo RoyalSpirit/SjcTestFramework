ac3318552f445a4d6c6140166c936aca20b096cb
RoyalSpirit <mettour@yandex.ru>
2025-06-07 10:23:47 +0300

    Добавлен отчет allure и прочие мелочи

--- src/test/java/sjc/aft/framework/core/Setup.java
+++ src/test/java/sjc/aft/framework/core/Setup.java
@@ -1,16 +1,22 @@
 package sjc.aft.framework.core;
 
 import com.codeborne.selenide.Configuration;
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeAll;
+import com.codeborne.selenide.Selenide;
+import io.cucumber.java.*;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
 import static com.codeborne.selenide.Selenide.*;
+import static sjc.aft.framework.pages.AbstractPage.ANSI_CYAN;
+import static sjc.aft.framework.pages.AbstractPage.ANSI_RESET;
 
 
 public class Setup {
@@ -18,6 +24,17 @@ public class Setup {
     public static final Logger logger = LoggerFactory.getLogger(Setup.class);
 
     @BeforeAll
+    public static void registerAllPages() throws Exception {
+        PageContextRegistry.autoRegisterPages("sjc.aft.framework.pages");
+    }
+
+    // Enables Selenide logger
+//    @Before
+//    public void setupAllureReports() {
+//        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+//    }
+
+    @Before
     public static void sepUp() throws Exception {
         Configuration.browser = GetPropertyValues.getProperty("browser.name");
         Configuration.browserSize = GetPropertyValues.getProperty("browser.size");
@@ -31,15 +48,28 @@ public class Setup {
             // Custom path to Firefoxdriver
 //        System.setProperty("webdriver.gecko.driver", Objects.requireNonNull(GetPropertyValues.getProperty("path.to.webdriver")));
         } else throw new Exception("Webdriver not found or not set in configuration");
+    }
 
+    @Before
+    public void scenarioNameLogging(Scenario scenario) {
+        logger.info("Running scenario with name: " + ANSI_CYAN + scenario.getName() + ANSI_RESET);
+    }
+
+    @Before
+    public void opensStartingUrl() throws Exception {
         String startingUrl = GetPropertyValues.getProperty("starting.url");
         open(startingUrl);
         logger.info("Opening URL: " + startingUrl);
     }
 
-    @Before
-    public void registerAllPages() throws Exception {
-        PageContextRegistry.autoRegisterPages("sjc.aft.framework.pages");
+    @After
+    public void takeScreenshotIfTestFails(Scenario scenario) throws IOException {
+        if (scenario.isFailed()) {
+            byte[] screenshot = Selenide.screenshot(OutputType.BYTES);
+            try (InputStream inputStream = new ByteArrayInputStream(screenshot)) {
+                Allure.attachment("Failed step screenshot", inputStream);
+            }
+        }
     }
 
     @AfterAll
--- src/test/resources/features/firstFeature.feature
+++ src/test/resources/features/firstFeature.feature
@@ -3,8 +3,8 @@ Feature: First Test Feature
   @TEST-001
   Scenario: TEST-001 User login and check list of products
   * user is on page "Page Swag Labs"
-  * user (fills field) "Username" значением "standard_user"
-  * user (fills field) "Password" значением "secret_sauce"
+  * user (fills field) "Username" with value "standard_user"
+  * user (fills field) "Password" with value "secret_sauce"
   * user (press button) "Login"
   * user is on page "Products"
   * user (checks list of elements) "Products list" with data:
